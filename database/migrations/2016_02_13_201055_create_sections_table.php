7f49564466413a0eb69efa4748f3d66a5bce341e
Kedar <kedar54@gmail.com>
2016-02-13 15:12:31 -0500

    course and section migration

---
+++ database/migrations/2016_02_13_201055_create_sections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('course_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+}
