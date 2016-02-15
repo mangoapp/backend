5a4b110dc6747b1f638d90a4908c6e313326f5c2
Kedar <kedar54@gmail.com>
2016-02-15 12:57:54 -0500

    fix controller naming

--- app/Http/Controllers/UsersController.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Http\Controllers;
-
-use App\Http\Controllers\Controller;
-
-class UsersController extends Controller {
-	public function showWelcome() {
-		return "ok";
-	}
-}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'UserController@showWelcome');
 /*
 |--------------------------------------------------------------------------
 | Application Routes
