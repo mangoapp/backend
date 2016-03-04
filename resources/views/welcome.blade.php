b05ee010de67acc2c48cb50c2d11f626301b3955
Kedar <kedar54@gmail.com>
2016-03-03 20:24:29 -0500

    get dat accent in there

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Mango</div>
+                <div class="title">Mangó</div>
             </div>
         </div>
     </body>
