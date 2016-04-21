7c705e4eebe4a2e6abb0d09db6118cfad75a25d4
Jacob Brabec <jbrabec@purdue.edu>
2016-04-20 20:49:12 -0400

    updated seeder

--- database/seeds/AnnouncementsTableSeeder.php
+++ database/seeds/AnnouncementsTableSeeder.php
@@ -14,7 +14,8 @@ class AnnouncementsTableSeeder extends Seeder
         $data = array(
             ['id' => 1, 'user_id' => 1, 'section_id' => 1, 'title' => 'Good news, Everyone!', 'body' => 'We will be having a surprise midterm tomorrow! I hope you guys all studied!',  'created_at' => new DateTime(), 'updated_at' => new DateTime()],
             ['id' => 2, 'user_id' => 1, 'section_id' => 1, 'title' => 'Exam Graded', 'body' => 'The good news is that I finished grading the exam! The bad news is that the average was a 17%. The ugly news is that I still won\'t curve it.', 'created_at' => new DateTime(), 'updated_at' => new DateTime()],
-            ['id' => 3, 'user_id' => 1, 'section_id' => 1, 'title' => 'Homework Problems', 'body' => 'Some people have been saying the homework is too hard. Therefore, I decided that everyone will get an automatic A!', 'created_at' => new DateTime(), 'updated_at' => new DateTime()]
+            ['id' => 3, 'user_id' => 1, 'section_id' => 1, 'title' => 'Homework Problems', 'body' => 'Some people have been saying the homework is too hard. Therefore, I decided that everyone will get an automatic A!', 'created_at' => new DateTime(), 'updated_at' => new DateTime()],
+            ['id' => 4, 'user_id' => 1, 'section_id' => 2, 'title' => 'Section Update', 'body' => 'This is a section for just the cool students. If you are reading this, you are super cool!', 'created_at' => new DateTime(), 'updated_at' => new DateTime()]
         );
         DB::table('announcements')->insert($data);
     }
